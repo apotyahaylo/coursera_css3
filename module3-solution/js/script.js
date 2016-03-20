2cf3f717457835f8f72d301dcc537e897ec6e358
Andriy Potyahaylo <apotyahaylo@gmail.com>
2016-03-20 15:48:11 +0200

    some impovements-2

--- module3-solution/js/script.js
+++ module3-solution/js/script.js
@@ -1,4 +1,4 @@
-$('.nav a').on('click', function(){
-//    $('#menu-button').click(); //bootstrap 2.x
-    $('.navbar-toggle').click() //bootstrap 3.x by Richard
-});
+// $('.nav a').on('click', function(){
+// //    $('#menu-button').click(); //bootstrap 2.x
+//     $('.navbar-toggle').click() //bootstrap 3.x by Richard
+// });
